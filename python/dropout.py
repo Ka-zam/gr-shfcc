731a41339220ec2ef69fe59b5765f2a3ca9e8b22
Magnus Lundmark <magnus@skysense.io>
2018-11-25 11:48:27 +0100

    alsacap and dropout detection

--- python/dropout.py
+++ python/dropout.py
@@ -4,8 +4,15 @@ import matplotlib as mpl
 from matplotlib.pyplot import plot,show,hold,grid
 from scipy.io import wavfile
 import numpy as np
+import scipy as sp
+import scipy.signal as sig
 import sys
 
+def db20(x):
+	return 20.*np.log10(x)
+
+def db10(x):
+	return 10.*np.log10(x)
 
 fname = sys.argv[1]
 
@@ -63,13 +70,16 @@ freq = 1./period
 
 #Fit suitable models to the data. 
 
-u_p = interp1d(u_x,u_y, kind = 'quadratic',bounds_error = False, fill_value=0.0)
-l_p = interp1d(l_x,l_y,kind = 'quadratic',bounds_error = False, fill_value=0.0)
+u_p = interp1d(u_x,u_y, kind = 'linear',bounds_error = False, fill_value=0.0)
+l_p = interp1d(l_x,l_y,kind = 'linear',bounds_error = False, fill_value=0.0)
 
 #Evaluate each model over the domain of (s)
 for k in xrange(0,len(s)):
     q_u[k] = u_p(k)
     q_l[k] = l_p(k)
+
+
+
 
 #Plot everything
 mpl.pyplot.figure(1)
@@ -77,11 +87,19 @@ grid(True)
 plot(s)
 plot(q_u,'r')
 plot(q_l,'g')
-print("Mean period    %8.3f us" % np.mean(period*1.e6))
-print("Std  period    %8.3f us" % np.std(period*1.e6))
 
-print("Mean frequency %8.3f Hz" % np.mean(freq))
-print("Std  frequency %8.3f Hz" % np.std(freq))
+#Calculate dropouts
+q_u = q_u[50:-50]
+print("Mean upper envelope: %8.3f" % np.mean(q_u) )
+trsh = np.mean(q_u)*.95
+
+plot( (0,len(s)) , (trsh,trsh) )
+
+print("Mean period   : %8.3f us" % np.mean(period*1.e6))
+print("Std  period   : %8.3f us" % np.std(period*1.e6))
+
+print("Mean frequency: %8.3f Hz" % np.mean(freq))
+print("Std  frequency: %8.3f Hz" % np.std(freq))
 
 print("Have %d periods from %d samples, %8.3f ms" % (len(freq), len(s), len(s)/(fs*1.e-3) ))
 
@@ -111,8 +129,39 @@ grid(True)
 
 mpl.pyplot.figure(4)
 
-mpl.pyplot.psd(s,NFFT=2048,Fs=fs,scale_by_freq=True,noverlap=1024)
+pbins , Pxx = sig.welch(s, fs=fs, nperseg=16384, noverlap=None)  
+fbin = pbins[1]-pbins[0]
+
+plot(pbins,10*np.log10(Pxx))
+grid(True)
+
+idx = np.argmax(Pxx)
+pwr_w = 20
+sigpwr = np.sqrt(np.sum(Pxx[idx-pwr_w:idx+pwr_w+1])*fbin)
+
+idx_3 = idx*3
+h3pwr = np.sqrt(np.sum(Pxx[idx_3-pwr_w:idx_3+pwr_w+1])*fbin)
+
+idx_5 = idx*5
+if idx_5+pwr_w < len(Pxx):
+	h5pwr = np.sqrt(np.sum(Pxx[idx_5-pwr_w:idx_5+pwr_w+1])*fbin)
+else:
+	h5pwr = 1.e-20
 
 
+totpwr = np.sqrt(np.sum(Pxx)*fbin)
+snr =  sigpwr/(totpwr-sigpwr-h3pwr-h5pwr) 
 
-show()
+print("Signal power  : %8.3f dB" % db10(sigpwr))
+print("3rd harmonic  : %8.3f dBc" % db10(h3pwr/sigpwr))
+print("5th harmonic  : %8.3f dBc" % db10(h5pwr/sigpwr))
+print("Total  power  : %8.3f dB" % db10(totpwr))
+print("SNR  dB       : %8.3f dB" % db10(snr) )
+cap = 12.e3*np.log2(1+snr)
+print("Shannon capacity: %8.3f bps" % cap )
+
+
+PLOT = False
+
+if PLOT:
+	show()
