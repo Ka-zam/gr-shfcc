35db2c33ffc649b1eb9f9a590735d68c16d87d89
Magnus Lundmark <magnus@skysense.io>
2019-03-02 22:42:42 +0100

    sine calcs

--- matlab/fftsym.m
+++ matlab/fftsym.m
@@ -1,27 +1,30 @@
-function out = fftsym( f_p , f_s , x )
+function [out,period_est] = fftsym( f_p , f_s , x )
 % Generate a corrected symbol clock from a 
 % pilot tone. samples in x
 
 pha_adv_ratio = f_s/f_p;
-pha_inst = 0.;
-out  = zeros( size(x) );
+pha_inst = 1.;
 
-period_est = 6; %16kHz sampled at 96e3;
+period_est(1) = 96/f_p; %fs = 96e3;
 period_start = 0;
-%Per sample phase advance
-pha_adv = 2*pi/period_est*pha_adv_ratio;
+lag=2*ceil(period_est(end));
+out  = zeros( length(x)+lag,1 );
 
-for ii=1:length(x)
-    if x(ii) < 0. && x(ii+1) > 0.
-        %Positive zero crossing
+%Per sample phase advance
+pha_adv = 2*pi/period_est(end)*pha_adv_ratio;
+
+for ii=1:length(x)-1
+    if sign(x(ii)) ~= sign(x(ii+1))
+        % Zero crossing
         tmp = zerocross( ii , x(ii) , x(ii+1) );
-        period_est = tmp - period_start;
+        period_est(end+1) = 2*(tmp - period_start);
         period_start = tmp;
-        pha_adv = 2*pi/period_est*pha_adv_ratio;
+        pha_adv = 2*pi/period_est(end)*pha_adv_ratio;
     end
-    out(ii) = cos( pha_inst );
+    out(ii+1*lag-1) = cos( pha_inst );
     pha_inst = pha_inst + pha_adv;
 end
+out( length(x) ) = cos( pha_inst );
 
 function alpha = zerocross( idx , xn , xnp1 )
 alpha = idx - xn/(xnp1-xn);
--- matlab/sinepn.m
+++ matlab/sinepn.m
@@ -6,6 +6,7 @@ var0_vec = var0*randn(N,1);
 var3_vec = var3*randn(N,1);
 
 pha = zeros(size(var0_vec));
+pha(1) = var3_vec(1);
 
 for k=2:length(pha)
     pha(k) = pha(k-1) + var3_vec(k);
