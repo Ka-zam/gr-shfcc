b8d7be6d54117d9fb7d4da396b7b5cc437e7a7ac
Magnus Lundmark <magnus@skysense.io>
2019-03-03 03:27:42 +0100

    fixes

--- matlab/sinepn.m
+++ matlab/sinepn.m
@@ -1,17 +1,13 @@
 function [out,t,pha] = sinepn(fc,fs, var0 , var3 , N)
+%DRM800-A typical values:
+% var0 = 0.015
+% var3 = 0.0045
 
 t = linspace(0,(N-1)/fs,N)';
 
 var0_vec = var0*randn(N,1);
-var3_vec = var3*randn(N,1);
+var3_vec = cumsum(var3*randn(N,1));
 
-pha = zeros(size(var0_vec));
-pha(1) = var3_vec(1);
-
-for k=2:length(pha)
-    pha(k) = pha(k-1) + var3_vec(k);
-end
-
-pha = pha + var0_vec;
+pha = var3_vec + var0_vec;
 
 out = sin(2*pi*fc*t + pha);
