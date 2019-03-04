813e950b91461cab6e0c205219de4e34c1e3b380
Magnus Lundmark <magnus@skysense.io>
2019-03-04 02:19:34 +0100

    Ye estimator works

---
+++ matlab/amest.m
@@ -0,0 +1,83 @@
+function [f,m,d] = amest(X,Alg,Q)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% This is an implemenation of Aboutanios and Mulgrew (A&M) Estimators from the 
+% following reference:
+%
+% [1]. E. Aboutanios and B. Mulgrew, "Iterative Frequency Estimation by 
+%      Interpolation on Fourier Coefficients", IEEE Transactions on Signal
+%      Processing, vol. 53, no. 4, pp. 1237-1242, April 2005.
+%
+%
+% Input: 
+%   X       Complex input signal vector (row or column). If X is a matrix, the estimator is
+%   applied down the columns.
+%   Alg    Algorithm to use: If Alg=1, the first algorithm is used (that is
+%   the default), if Alg=2, the second is used.
+%   Q       The number of iterations to run (default = 2).
+% Output:
+%   f       Normalised frequency estimate (-0.5<=f<0.5)
+%   m     Index of the maximum bin (-N/2:N/2-1)
+%   d      Matrix of residuals (-0.5<=d<0.5).
+%
+% Copyright by Dr. E. Aboutanios, 03/2011.
+%
+% Email: elias@ieee.org
+%
+% Version: v1.0
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Error checking for the input parameters to the A&M estimator
+if nargin < 1
+     error('Function must have at least one input. Type help amest for instructions.');
+elseif nargin==1
+    Alg = 1;
+    Q = 2;
+elseif nargin == 2
+    Q = 2;
+end
+
+[N,M] = size(X);
+
+if (N==1) && (M>1)
+    X = X(:);
+    N = M;
+    M = 1;
+end
+
+nv = (0:N-1).';
+
+%---------------------------------------------------
+%Iterative A&M Algorithm - Q iterations
+%---------------------------------------------------
+
+Y = fft(X); %Obtain the DFT
+
+[~,m] = max(abs(Y)); %Find maximum bin
+
+
+mh = m-1;  %adjust from MATLAB notation to actual index
+
+d = zeros(Q,M);
+
+Xn = exp(-1i*2*pi*nv*(mh-0.5)/N).*X;
+Xp = exp(-1i*2*pi*nv*(mh+0.5)/N).*X;
+ 
+ for niter = 1:Q
+    
+    D = exp(-1i*2*pi*nv*sum(d)/N);
+    Yn = sum(D.*Xn);
+    Yp = sum(D.*Xp); 
+
+    if Alg == 1
+        d(niter,:) =  0.5*real((Yp+Yn)./(Yp-Yn));
+    elseif Alg == 2
+        d(niter,:) =  0.5*(abs(Yp)-abs(Yn))./(abs(Yp)+abs(Yn));
+    end
+    
+ end
+
+m = (mh>=N/2).*(mh-N)+(mh<N/2).*mh; %Calculate the index between -N/2 and N/2-1
+
+f = (m+sum(d))/N;
--- matlab/yeest.m
+++ matlab/yeest.m
@@ -1,6 +1,8 @@
 function out = yeest(x, Q)
 %Estimate f, A and phi for a real sinusoid
-% x = A*sin(2*pi*f*t + phi)
+% x = abs(A)*cos( 2*pi*f*t + angle(A) ) + w
+% where w is Gaussian with variance sigma^2 and
+% SNR = abs(A)^2/(2*sigma^2)
 
 if nargin < 2
     Q = 3;
@@ -10,14 +12,10 @@ x = x(:);
 
 N = length(x);
 X = fft(x);
-Y = abs(X).^2;
+Y = X.*conj(X);
 
-[~,m] = max(Y);
+[~,m] = max(Y(1:ceil(N/2)));
 m = m - 1; %Matlab indexing starts at 1
-
-if m >= N/2
-    m = N - m;
-end
 
 n_all = [0:N-1]';
 
@@ -25,19 +23,20 @@ delta = 0;
 A = 0;
 
 for i=1:Q
-    Xp = sum(x.*exp(-1j*2*pi/N*(m+0.5)*n_all));
-    Xn = sum(x.*exp(-1j*2*pi/N*(m-0.5)*n_all));
+
+    Xp05 = 1/N*sum( x.*exp(-1j*2*pi/N*(m+delta+0.5)*n_all) );%According to corrections
+    Xn05 = 1/N*sum( x.*exp(-1j*2*pi/N*(m+delta-0.5)*n_all) );%According to corrections
     
-    Stp = A*(1+exp(1j*4*pi*delta))/( 1- exp(-1j*2*pi/N*(2*m+2*delta+.5)) );
-    Stn = A*(1+exp(1j*4*pi*delta))/( 1- exp(-1j*2*pi/N*(2*m+2*delta-.5)) );
+    Lp05 = conj(A)/N*( 1+exp(-1j*4*pi*delta) )/( 1 - exp(-1j*2*pi/N*(2*m+2*delta+.5)) );
+    Ln05 = conj(A)/N*( 1+exp(-1j*4*pi*delta) )/( 1 - exp(-1j*2*pi/N*(2*m+2*delta-.5)) );
     
-    Sp = Xp - Stp;
-    Sn = Xn - Stn;
+    Sp05 = Xp05 - Lp05;
+    Sn05 = Xn05 - Ln05;
     
-    delta = delta + .5*real( (Sp + Sn)/(Sp - Sn) );
-    A = ( sum(x.*exp(1j*2*pi/N*(m+delta)*n_all)) -...
-        A*(1-exp(-1j*4*pi*delta))/(1-exp(-1j*4*pi/N*(m+delta))) )/N;
+    delta = delta + .5*real( (Sp05 + Sn05)/(Sp05 - Sn05) );
+    
+    A = 1/N*( sum( x.*exp( -1j*2*pi/N*(m+delta)*n_all ) ) -...
+        conj(A)*(1-exp(-1j*4*pi*delta))/(1-exp(-1j*4*pi/N*(m+delta))) );
 end
 
 out = [ (m+delta)/N 2*abs(A) angle(A) ];
-    
