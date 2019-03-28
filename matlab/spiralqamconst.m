af3e52c8926fb78940ffb7460e9e4ca6b798e44b
Magnus Lundmark <magnus@skysense.io>
2019-03-28 16:10:06 +0100

    Average power normalization.

--- matlab/spiralqamconst.m
+++ matlab/spiralqamconst.m
@@ -4,4 +4,6 @@ m = [1:M]';
 tm = sqrt( (4*pi*m).^2*R/2 + sqrt( (4*pi*m).^2*R^2/4 + (4*pi*m).^2 ) );
 
 c = tm.*exp(1j*tm);
-c = c/max(abs(c));
+n = sqrt( sum(c.*conj(c))/length(c) );
+c = c/n;
+%c = c/max(abs(c));
--- python/spiralqam.py
+++ python/spiralqam.py
@@ -4,17 +4,25 @@ from gnuradio.digital  import qam_constellation
 class spiralqam():
 
 	def __init__( self , N = 4 , R = 0.01 ):
-		self.points = self.calc_pts( N , R )
-		self.dimensionality = self.calc_dim()
+		self.pts = self.calc_pts( N , R )
+		self.dim = self.calc_dim( N , R )
 
-	def calc_dim( self ):
+	def points( self ):
+		return self.pts
+
+	def dimensionality( self ):
+		return self.dim
+
+	def calc_dim( self , N , R ):
 		return 1
 
 
-	def calc_pts( self , N=4 , R=0.01 ):
+	def calc_pts( self , N , R ):
 		m  =np.arange(1,N+1)
 		tm = np.sqrt( (4*np.pi*m)**2*R*.5 + np.sqrt( (4*np.pi*m*R*.5)**2 + (4*np.pi*m)**2 )  )
 		points = tm*np.exp(1j*tm)
-		points /= np.max(np.abs(points))
+		#n = sqrt( sum(c.*conj(c))/length(c) );
+
+		points /= np.sqrt( np.sum( points*np.conj(points) )/len(points) )
 		return tuple(points)
 	
