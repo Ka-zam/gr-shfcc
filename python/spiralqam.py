04f72eb108f24041688f70a51b6b56c0c2ab1155
Magnus Lundmark <magnus@skysense.io>
2019-04-01 02:21:01 +0200

    More modem pilot stuff

--- python/spiralqam.py
+++ python/spiralqam.py
@@ -1,5 +1,14 @@
 import numpy as np
+from scipy import signal
 from gnuradio.digital  import qam_constellation
+
+def sinepulse( Nper = 3, fc = 1000.0 , fs = 96000.0 ):
+	period = fs/fc
+	N = int( np.ceil(period*Nper) )
+	t = np.linspace( 0 , (N-1)/fs , N )
+
+	return np.sin(2*np.pi*fc*t) * signal.triang(N)
+
 
 class spiralqam():
 
@@ -16,6 +25,8 @@ class spiralqam():
 	def calc_dim( self , N , R ):
 		return 1
 
+	def bits_per_symbol( self ):
+		return np.log2(len(self.pts))
 
 	def calc_pts( self , N , R ):
 		m  =np.arange(1,N+1)
