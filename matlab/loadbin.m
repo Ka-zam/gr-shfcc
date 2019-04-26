f725a72864cbd858214a4dfe95ae4c5b45acfd66
Magnus Lundmark <magnus@skysense.io>
2019-04-26 17:01:12 +0200

    loadbin handles complex data

--- matlab/loadbin.m
+++ matlab/loadbin.m
@@ -1,9 +1,18 @@
 function d=loadbin(fname,vec_len,format)
 % function d=loadbin(fname,vec_len,format)
 % Load binary data
+
 if nargin < 3
     format = 'float32';
 end
+    
+if strcmp(format,'fc32')
+    ISCOMPLEX = true;
+    format = 'float32';    
+else
+    ISCOMPLEX = false;
+end
+    
 if nargin < 2
     vec_len = 1;
 end
@@ -12,4 +21,6 @@ f = fopen(fname,'r');
 d = fread(f,format);
 fclose(f);
 d = reshape(d,vec_len,[])';
-
+if ISCOMPLEX
+    d = d(1:2:end,:) + 1j*d(2:2:end,:);
+end
